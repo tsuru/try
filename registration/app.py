f20513ae9a2cab0e67b9327e3bb2f9f042ad4468
Andrews Medina <andrewsmedina@gmail.com>
2014-05-09 17:15:41 -0300

    added signup form and index template.

--- registration/app.py
+++ registration/app.py
@@ -1,9 +1,15 @@
 import flask
+from flask import render_template, request
+
+from .forms import SignupForm
 
 
 app = flask.Flask(__name__)
+app.secret_key = "secret?"
+app.debug = True
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "tsuru"
+    form = SignupForm(request.form)
+    return render_template("index.html", form=form)
