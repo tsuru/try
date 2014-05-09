f02cf687dba842a1cea808ec9389e3ede24832a2
Andrews Medina <andrewsmedina@gmail.com>
2014-05-09 14:13:56 -0300

    forms: added SignupForm.

---
+++ registration/forms.py
@@ -0,0 +1,10 @@
+from flask.ext import wtf
+import wtforms
+
+
+class SignupForm(wtf.Form):
+    email = wtforms.TextField(u"Email")
+    password = wtforms.PasswordField(u"Password")
+    password_confirmation= wtforms.PasswordField(u"Password confirmation")
+    identity = wtforms.TextField(u"National Identification Number")
+    terms = wtforms.BooleanField(u"I accept the Terms")
---
+++ tests/test_forms.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from flask.ext import wtf
+import wtforms
+
+from registration.forms import SignupForm
+
+
+class SignupFormTestCase(TestCase):
+    def test_should_be_a_wtform(self):
+        self.assertTrue(issubclass(SignupForm, wtf.Form))
+
+    def test_fields(self):
+        fields = {
+            "email": wtforms.TextField,
+            "password": wtforms.PasswordField,
+            "password_confirmation": wtforms.PasswordField,
+            "identity": wtforms.TextField,
+            "terms": wtforms.BooleanField,
+        }
+        for field, cls in fields.items():
+            self.assertEqual(cls, getattr(SignupForm, field).field_class)
